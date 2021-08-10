6cc59af4481a60c59cf124a58b039bc6f2657063
claudiah85 <64367231+claudiah85@users.noreply.github.com>
2021-08-10 15:26:28 -0300

    .

--- R/nuevo_codigo.R
+++ R/nuevo_codigo.R
@@ -2,4 +2,9 @@ pop<-gapminder::gapminder
 
 summary(pop)
 
-#otro cambio
+data(MASS::cats)
+ pdf("mis_imagenes/cats_plot.pdf") 
+ggplot(cats, aes(x = Sex)) + 
+  geom_bar(fill = "orange", color = "black") + theme_classic() +
+  xlab("Sexo") + ylab("Número de Gatos") + ggtitle("Gatos")
+dev.off()
